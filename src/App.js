374e5ab0069150ef127deb7c0a358fea4caa9471
robertchrobak <26276474+robertchrobak@users.noreply.github.com>
2017-11-27 00:42:57 -0500

    Small tweaks to initial project setup. Deleted unnecessary files.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Anthony, Cecil & Robert's Final Project</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This is the beginning of something special
         </p>
       </div>
     );
