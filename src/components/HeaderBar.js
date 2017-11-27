438b62d497b5b2fc1187ff0b9d0a8f45dd12f062
Cecil Johnson <cecil344@gmail.com>
2017-11-27 14:05:32 -0500

    styled header bar

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -3,7 +3,15 @@ import React, {Component} from 'react';
 class HeaderBar extends Component {
   render() {
     return (
-      <p>Welcome, Meal Plan, etc</p>
+      <div class="headerWrapper">
+        <div class="headerLeft">
+          <h1> TODAY'S MEAL PLAN</h1>
+        </div>
+
+        <div class="headerRight">
+          <p class= "caloricGoal">Caloric Goal: 1700</p>
+        </div>
+      </div>
     );
   }
 }
