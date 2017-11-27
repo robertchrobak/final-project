1fb6e005b51b2bb2183fb42bef42622c800d8dc7
Anthony Iamurri <anthony.iamurri@gmail.com>
2017-11-27 14:05:20 -0500

    added css styling to mealplan & progress bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -3,7 +3,10 @@ import React, {Component} from 'react';
 class ProgressBar extends Component {
   render() {
     return (
-      <p>Progress bar</p>
+      <div className="ProgressBar">
+        <h2>Progress bar</h2>
+      <progress id="ProgressBar" value="1700" max="1800"></progress>
+    </div>
     );
   }
 }
