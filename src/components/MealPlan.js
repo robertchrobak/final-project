1f84457dd1d0390ebefcbdcb106b7c79d528cda0
robertchrobak <26276474+robertchrobak@users.noreply.github.com>
2017-11-30 11:21:27 -0500

    Adding items to mealPlan divs almost completely works

--- src/components/MealPlan.js
+++ src/components/MealPlan.js
@@ -2,17 +2,32 @@ import React, {Component} from 'react';
 import { connect } from 'react-redux';
 
 class MealPlan extends Component {
-  render() {
-    const meal1Items = this.props.foodItems.map((foodItem, index) =>
-        <li key={index}>{foodItem.food} - {foodItem.calories} calories</li>
-    );
 
-    const meal2Items = this.props.foodItems.map((foodItem, index) =>
-        <li key={index}>{foodItem.food} - {foodItem.calories} calories</li>
-    );
+  render() {
+
+    const meal1Items = [];
+    const meal2Items = [];
+    const meal3Items = [];
+    const snackItems = [];
+
+    this.props.foodItems.forEach((foodItem, index) => {
+
+      if(foodItem.meal === "Meal 1") {
+        meal1Items.push(<li key={index}>{foodItem.food} - {foodItem.calories} calories</li>);
+      }
+      if(foodItem.meal === "Meal 2") {
+        meal2Items.push(<li key={index}>{foodItem.food} - {foodItem.calories} calories</li>);
+      }
+      if(foodItem.meal === "Meal 3") {
+        meal3Items.push(<li key={index}>{foodItem.food} - {foodItem.calories} calories</li>);
+      }
+      if(foodItem.meal === "Snacks") {
+        snackItems.push(<li key={index}>{foodItem.food} - {foodItem.calories} calories</li>);
+      }
+
+    });
 
     return (
-
       <div className="MealPlan">
         <div className="Meal">
           <h2>Meal 1</h2>
@@ -29,11 +44,13 @@ class MealPlan extends Component {
         <div className="Meal">
           <h2>Meal 3</h2>
           <ul>
+            {meal3Items}
           </ul>
         </div>
         <div className="Meal">
           <h2>Snacks</h2>
           <ul>
+            {snackItems}
           </ul>
         </div>
       </div>);
@@ -45,6 +62,5 @@ function mapStateToProps(state) {
         foodItems: state.foodItems
     }
 }
-
 
 export default connect(mapStateToProps)(MealPlan);
