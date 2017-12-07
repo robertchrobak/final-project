2fae8344f04fc0479a7e42850ddd4e9385ae65d4
Anthony Iamurri <anthony.iamurri@gmail.com>
2017-12-07 14:23:41 -0500

    styled chart js lookin faf saf

--- src/components/CalorieGoalForm.js
+++ src/components/CalorieGoalForm.js
@@ -17,7 +17,7 @@ class CalorieGoalForm extends Component {
           <input type="number" className="calorieGoal" placeholder="Today's calorie goal" value={this.state.calorieGoal} onChange={this.handleCalorieGoalChange.bind(this)}/>
           <button className="enterButton" type="submit">ENTER</button>
           </form>
-          <a href="https://www.bodybuilding.com/fun/macronutcal.htm" target="blank" class="targetLink">Don't know your calorie goal?</a>
+          <a href="https://www.bodybuilding.com/fun/macronutcal.htm" target="blank" className="targetLink">Don't know your calorie goal?</a>
       </div>
     );
   }
--- src/components/Chart.js
+++ src/components/Chart.js
@@ -1,73 +1,118 @@
 import React, {Component} from 'react';
 import {Bar} from 'react-chartjs-2';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
 //since we have data, we're going to need it in a state
-class Chart extends Component{
+class Chart extends Component {
 
-  render () {
+  render() {
 
-      let calculatedTotal = 0;
-      this.props.foodItems.forEach((foodItem) =>
-        calculatedTotal+=foodItem.calories
-      );
+    let calculatedTotal = 0;
+    this.props.foodItems.forEach((foodItem) => calculatedTotal += foodItem.calories);
 
     var chartData = {
-          labels: ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'],
-          datasets:[
-            {
-              label: 'Calories consumed',
-              data:[
-                1800 ,
-                1900,
-                2000,
-                1800,
-                1500,
-                parseInt(calculatedTotal),
-                0
-              ],
-              backgroundColor: ['#fff', '#fff', '#fff', 'white', 'white', 'white'],
-              hoverBackgroundColor: ['rgba(182,100,24,1)', 'rgba(182,100,24,1)','rgba(182,100,24,1)','rgba(182,100,24,1)','rgba(182,100,24,1)','rgba(182,100,24,1)', 'rgba(182,100,24,1)'],
-              yAxes: [{
-                           display: true,
-                           ticks: {
-                              beginAtZero: true,
-                               // steps: 10,
-                               // stepValue: 5,
-                               // max: 100
-                           }
-                       }],
-            }
+      labels: [
+        'Sun',
+        'Mon',
+        'Tues',
+        'Wed',
+        'Thurs',
+        'Fri',
+        'Sat'
+      ],
+      datasets: [
+        {
+          label: 'Calories consumed',
+          data: [
+            1800,
+            1900,
+            2000,
+            1800,
+            1500,
+            parseInt(calculatedTotal),
+            0
           ],
-        }
-
-    return (
-      <div className="chart">
-        <Bar
-          data={chartData} //calling the data that's in the state
-          options={{
-            responsive: true,
-            barThickness: 20,
-            // maintainAspectRatio: false,
-            title: {
+          fontColor: 'rgba(184,101,24,1)',
+          backgroundColor: [
+            'white',
+            '#fff',
+            '#fff',
+            'white',
+            'white',
+            'white'
+          ],
+          hoverBackgroundColor: [
+            'rgba(182,100,24,1)',
+            'rgba(182,100,24,1)',
+            'rgba(182,100,24,1)',
+            'rgba(182,100,24,1)',
+            'rgba(182,100,24,1)',
+            'rgba(182,100,24,1)',
+            'rgba(182,100,24,1)'
+          ],
+          yAxes: [
+            {
               display: true,
-              text: 'Weekly Progress',
-              fontSize: 25,
-              fontColor: 'black',
-              fontFamily: 'Lato'
-            },
+              ticks: {
+                beginAtZero: true,
 
-          }}
-        />
-      </div>
-    )
+                // steps: 10,
+                // stepValue: 5,
+                // max: 100
+              }
+            }
+          ]
+
+        }
+      ]
+    }
+
+    return (<div className="chart">
+      <Bar data={chartData}
+        //calling the data that's in the state
+        options={{
+          responsive: true,
+          barThickness: 20,
+          // maintainAspectRatio: false,
+          title: {
+            display: true,
+            text: 'Weekly Progress',
+            fontSize: 25,
+            fontColor: 'rgba(184,101,24,1)',
+            fontFamily: 'Lato'
+          },
+          legend: {
+            labels: {
+              fontColor: 'rgba(184,101,24,1)',
+            }
+          },
+          scales: {
+            yAxes: [{
+                gridLines: {
+                  color: 'rgba(251,163,58,1)',
+                  zeroLineColor: 'rgba(251,163,58,1)',
+                },
+                ticks: {
+                  fontColor: 'rgba(184,101,24,1)'
+                }
+
+              }],
+            xAxes: [{
+                gridLines: {
+                  color: 'rgba(251,163,58,1)',
+                },
+                ticks: {
+                  fontColor: 'rgba(184,101,24,1)'
+                }
+              }]
+            }
+        }}/>
+    </div>)
   }
 }
 
 function mapStateToProps(state) {
-    return {
-        foodItems: state.foodItems,
-    }
+  return {foodItems: state.foodItems}
 }
 
 export default connect(mapStateToProps)(Chart);
